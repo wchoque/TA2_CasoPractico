64334cbdf316f62d02fa56bd3bf4e546f14bf2d9
Luigi <luigi@bitso.com>
2022-10-26 17:08:53 -0500

    First changes

--- src/main/java/com/upc/salud/entidades/CentroSalud.java
+++ src/main/java/com/upc/salud/entidades/CentroSalud.java
@@ -1,4 +1,33 @@
 package com.upc.salud.entidades;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "CENTRO_SALUD")
 public class CentroSalud {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String nombre;
+    private String tipo;
+    private int calificacionInfraestructura;
+    private int calificacionServicios;
+    private int ambulancias;
+
+    public CentroSalud(){}
+
+    public CentroSalud(Long codigo, String nombre, String tipo, int calificacionInfraestructura, int calificacionServicios, int ambulancias) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.calificacionInfraestructura = calificacionInfraestructura;
+        this.calificacionServicios = calificacionServicios;
+        this.ambulancias = ambulancias;
+    }
 }
